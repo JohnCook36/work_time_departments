c65f6b12066b2c8ff78a2c07a0326e70c2d6f6f3
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-22 23:23:42 +0300

    feat(routes): add my schedule route and protect planner

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
 import { LoginPage } from '../pages/LoginPage';
-import { OnboardingPage } from '../pages/OnboardingPage';
+import { MySchedulePage } from '../pages/MySchedulePage';
 import { NotFoundPage } from '../pages/NotFoundPage';
+import { OnboardingPage } from '../pages/OnboardingPage';
 import { PlannerPage } from '../pages/PlannerPage';
-import { LoginRoute, OnboardingRoute, ProtectedRoute, SessionRoutes } from './SessionRoutes';
+import {
+  HomeRoute,
+  LoginRoute,
+  ManagementRoute,
+  OnboardingRoute,
+  ProtectedRoute,
+  SessionRoutes,
+} from './SessionRoutes';
 
 export function AppRouter() {
   return (
@@ -13,12 +22,19 @@ export function AppRouter() {
         <Route element={<LoginRoute />}>
           <Route path="/login" element={<LoginPage />} />
         </Route>
+
         <Route element={<OnboardingRoute />}>
           <Route path="/onboarding" element={<OnboardingPage />} />
         </Route>
+
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/planner" replace />} />
-          <Route path="/planner" element={<PlannerPage />} />
+          <Route path="/" element={<HomeRoute />} />
+          <Route path="/my-schedule" element={<MySchedulePage />} />
+
+          <Route element={<ManagementRoute />}>
+            <Route path="/planner" element={<PlannerPage />} />
+          </Route>
+
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Route>
