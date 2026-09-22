c5d15dca0e5bacd0b8bd0b6bef4482d4201bec90
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-22 22:55:30 +0300

    feat(routing): route employees to personal schedule

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
 import { OnboardingPage } from '../pages/OnboardingPage';
 import { NotFoundPage } from '../pages/NotFoundPage';
 import { PlannerPage } from '../pages/PlannerPage';
-import { LoginRoute, OnboardingRoute, ProtectedRoute, SessionRoutes } from './SessionRoutes';
+import { MySchedulePage } from '../pages/MySchedulePage';
+import {
+  AuthenticatedHomeRoute,
+  LoginRoute,
+  ManagementRoute,
+  OnboardingRoute,
+  ProtectedRoute,
+  SessionRoutes,
+} from './SessionRoutes';
 
 export function AppRouter() {
   return (
@@ -17,8 +25,11 @@ export function AppRouter() {
           <Route path="/onboarding" element={<OnboardingPage />} />
         </Route>
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/planner" replace />} />
-          <Route path="/planner" element={<PlannerPage />} />
+          <Route path="/" element={<AuthenticatedHomeRoute />} />
+          <Route path="/my-schedule" element={<MySchedulePage />} />
+          <Route element={<ManagementRoute />}>
+            <Route path="/planner" element={<PlannerPage />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Route>
