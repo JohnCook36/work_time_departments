3909167759bf84d7101781519552d36eb83b1575
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-22 22:43:58 +0300

    refactor(styles): add planner header styles

---
+++ src/screens/planner/PlannerHeaderScreen.styles.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+import { Card, Muted } from '../../theme/styles';
+
+export const ServerModeCard = styled(Card)({
+  marginTop: 14,
+  padding: 16,
+});
+
+export const ServerModeDescription = styled(Muted)({
+  marginTop: 4,
+});
