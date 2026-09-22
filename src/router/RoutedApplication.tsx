3440a2665cf36f61e690e7dcc3d12e65f6b95e5e
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-22 23:25:39 +0300

    refactor(theme): provide theme at routed application root

--- src/router/RoutedApplication.tsx
+++ src/router/RoutedApplication.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthSessionProvider } from '../auth/AuthSessionProvider';
 import { ErrorBoundary } from '../components/ErrorBoundary';
+import { AppThemeProvider } from '../theme/AppThemeProvider';
 import { AppRouter } from './AppRouter';
 
 export function RoutedApplication() {
   return (
     <ErrorBoundary>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <AuthSessionProvider>
-          <AppRouter />
-        </AuthSessionProvider>
-      </BrowserRouter>
+      <AppThemeProvider>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <AuthSessionProvider>
+            <AppRouter />
+          </AuthSessionProvider>
+        </BrowserRouter>
+      </AppThemeProvider>
     </ErrorBoundary>
   );
 }
