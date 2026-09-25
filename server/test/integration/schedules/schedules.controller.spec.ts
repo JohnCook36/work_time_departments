8bbc64d48f9d02d9aeb2514be4ffed5bc78e6564
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-25 17:41:41 +0300

    test(acknowledgement): satisfy schedules controller dependency

--- server/test/integration/schedules/schedules.controller.spec.ts
+++ server/test/integration/schedules/schedules.controller.spec.ts
@@ -8,6 +8,7 @@ import { AuthUserContext } from '../../../src/auth/auth.service';
 import { AuthorizationService } from '../../../src/auth/authorization.service';
 import { SessionAuthGuard } from '../../../src/auth/session-auth.guard';
 import { PrismaService } from '../../../src/prisma/prisma.service';
+import { ScheduleAcknowledgementsService } from '../../../src/schedules/schedule-acknowledgements.service';
 import { SchedulePublicationsService } from '../../../src/schedules/schedule-publications.service';
 import { SchedulesController } from '../../../src/schedules/schedules.controller';
 import { SchedulesService } from '../../../src/schedules/schedules.service';
@@ -91,6 +92,7 @@ describe('SchedulesController API validation', () => {
       providers: [
         SchedulesService,
         { provide: SchedulePublicationsService, useValue: publications },
+        { provide: ScheduleAcknowledgementsService, useValue: {} },
         { provide: PrismaService, useValue: prisma },
         { provide: AuthorizationService, useValue: authorization },
       ],
