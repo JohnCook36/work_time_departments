2c26a676e6c55ef953c8ed983c8553a6b9a99f0a
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-25 17:24:35 +0300

    feat(publication): wire notification producer

--- server/src/schedules/schedules.module.ts
+++ server/src/schedules/schedules.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { SchedulePublicationsService } from './schedule-publications.service';
 import { SchedulesController } from './schedules.controller';
 import { SchedulesService } from './schedules.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, NotificationsModule],
   controllers: [SchedulesController],
   providers: [SchedulesService, SchedulePublicationsService],
 })
