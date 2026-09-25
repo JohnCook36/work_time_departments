26ae607ba1482632084703f12051ae59d9e02d59
JohnCook36 <50880354+JohnCook36@users.noreply.github.com>
2026-09-25 17:17:34 +0300

    feat(shift-change): wire notification producers

--- server/src/shift-change-requests/shift-change-requests.module.ts
+++ server/src/shift-change-requests/shift-change-requests.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { ShiftChangeRequestsController } from './shift-change-requests.controller';
 import { ShiftChangeRequestsService } from './shift-change-requests.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, NotificationsModule],
   controllers: [ShiftChangeRequestsController],
   providers: [ShiftChangeRequestsService],
 })
